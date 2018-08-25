3821024ea036052e6e8b2e3f328f9a480bc03ff1
danieleduardo29 <danieleduardo29@gmail.com>
2018-08-25 18:38:50 -0500

    Added mock form to Reports

--- code/deliktum-frontend/src/app/reports/reports.component.ts
+++ code/deliktum-frontend/src/app/reports/reports.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { Report } from '../report';
 import { Observable } from 'rxjs/Observable';
 import { ReportsService } from '../reports.service';
