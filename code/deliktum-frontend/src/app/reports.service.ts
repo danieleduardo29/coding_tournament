877a5050af3779597437aa89ee6fb5a1b8fd04aa
danieleduardo29 <danieleduardo29@gmail.com>
2018-08-25 12:13:13 -0500

    Added the Angular frontend app

---
+++ code/deliktum-frontend/src/app/reports.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+
+export interface Report {
+	_id: number,
+	type: string,
+	description: string,
+	datetime: number,
+	location: {
+		lat: number,
+		lng: number,
+	},
+	image: string
+}
+
+@Injectable()
+export class ReportService {
+  constructor(private http: HttpClient) {}
+
+  getAllReports(): Observable<Report[]> {
+    return this.http.get<Report[]>('http://localhost:2901/api/reports');
+  }
+
+  getReport(id: number): Observable<Report> {
+    return this.http.get<Report>('http://localhost:2901/api/reports/' + id);
+  }
+
+  insertReport(Report: report): Observable<Report> {
+    return this.http.post<Report>('http://localhost:2901/api/reports/', report);
+  }
+}
